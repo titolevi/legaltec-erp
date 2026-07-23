321db1e0184e693246a9a9983a34816eae2d350f
Daniel León <titolevi@hotmail.com>
2026-07-22 23:47:46 -0500

    fix: Quitar databaseNotifications (tabla no existe)

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,8 +54,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->databaseNotifications()
-            ->databaseNotificationsPolling('30s');
+            ]);
     }
 }
